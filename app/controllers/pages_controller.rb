c0c56c58953825ecfcfcf7cd11ae72ab91184256
aleighbell <aleighbell@gmail.com>
2016-09-26 16:00:32 -0400

    established routes/pagescontroller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def index
+  end
+
+  def contact_us
+  end
+
+  def business
+  end
+
+  def signup
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-resources :welcome
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'pages#index'
+get 'welcome' => 'pages#index'
+get 'contact-us' => 'pages#contact_us'
+get 'business' => 'pages#business'
+get 'signup' => 'pages#signup'
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
