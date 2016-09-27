77429cf11c5115e00f07446154e00648f13ecc22
aleighbell <aleighbell@gmail.com>
2016-09-27 14:51:23 -0400

    database seeded successfully!

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,6 @@
 class Business < ApplicationRecord
   belongs_to :categories
   belongs_to :cities
-  has_and_belongs_to_many :users
-  has_many :favourites
+  belongs_to :users
+  has_many   :favourites
 end
