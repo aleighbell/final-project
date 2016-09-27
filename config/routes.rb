4446ccad907b97e2b411737b10f476da08db6da3
wes <weshenshall@gmail.com>
2016-09-27 17:36:24 -0400

    created business controller, created business_index page to view businesses, allows list of businesses to be viewed

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ get 'welcome' => 'pages#index'
 get 'contact-us' => 'pages#contact_us'
 get 'business' => 'pages#business'
 get 'signup' => 'pages#signup'
+get 'business_index' => 'business#index'
+resources :business
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
