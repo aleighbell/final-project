9be00e0cacfd59bc4af1371eb1afa2696dc6c01a
Darren.DV <darr.dv@gmail.com>
2016-09-29 16:24:45 -0400

    tweaked routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,13 @@ post 'contact-us' => 'pages#contact_save'
 get 'contact-us' => 'pages#contact_us'
 get 'about_us' => 'pages#about_us'
 get 'business' => 'pages#business'
-<<<<<<< HEAD
-get 'signup' => 'pages#signup'
+#get 'signup' => 'pages#signup'
 get 'business_index' => 'business#index'
 resources :business
-=======
-#get 'signup' => 'pages#signup'
 
 get '/sign_up', to: 'users#new', as: :sign_up
 get '/log_in', to: 'sessions#new', as: :log_in
 delete '/log_out', to: 'sessions#destroy', as: :log_out
 
->>>>>>> authentication
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
