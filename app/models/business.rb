7d624900bea789c35824caf55d653d651fe3a3e3
aleighbell <aleighbell@gmail.com>
2016-10-04 15:22:51 -0400

    added data

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,15 +2,11 @@ class Business < ApplicationRecord
   max_paginates_per 10
   belongs_to :category
   belongs_to :city
-  geocoded_by :business_address   # can also be an IP address
-  after_validation :geocode          # auto-fetch coordinates
+  #geocoded_by :business_address   # can also be an IP address
+  #after_validation :geocode, if: ->(business) { business.address.present? and business.address_changed? }          # auto-fetch coordinates
 
 end
 
-#def address
-# geocoded_by :address
-# after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
-#end
 
 
 #def display_distance_to(current_position)
