273cee59ef5e6162e1b8242b17c09056f03d7eea
wes <weshenshall@gmail.com>
2016-10-05 16:35:16 -0400

    no change

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,25 @@
+class ParksController < ApplicationController
+  def create
+  @park = Park.create(park_params )
+end
+
+  def index
+    @parks = Park.all
+    
+  end
+
+  def new
+    @park = Park.new
+  end
+
+
+
+private
+
+# Use strong_parameters for attribute whitelisting
+# Be sure to update your create() and update() controller methods.
+
+def park_params
+  params.require(:park).permit(:photo, :name, :description)
+end
+end
